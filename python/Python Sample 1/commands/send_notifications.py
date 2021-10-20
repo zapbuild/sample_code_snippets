8fe629ea0e6a17f0f0778063bd55e79b4ad4d787
Arpan <arpansharma@zapbuild.com>
2021-10-20 12:12:06 +0530

    del commands follllder

--- python/Python Sample 1/commands/send_notifications.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from account.crons import send_notifications
-
-class Command(BaseCommand):
-    help = 'Send pending notificaitons to the users'
-
-    def handle(self, *args, **options):
-        send_notifications()
